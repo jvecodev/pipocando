ec3602fd2b3d0b8a1af6fe90b65a0da052466cfd
Jcliz <joaopedrocliz@gmail.com>
2025-03-04 21:48:23 -0300

    template inicial do projeto

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './login/login';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
