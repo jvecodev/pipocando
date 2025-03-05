53269f6ca7fb7a43dd1d6c01a256d8f505b50f3b
Eduardo Fabri <eduardohfabri@gmail.com>
2025-03-05 13:46:54 -0300

    feat(blog): refatorar a estrutura dos componentes do blog e integrar o conteúdo

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { Container, Typography, Link, AppBar, Toolbar, Button, Box } from "@mui/material";
-import { FlashOn } from "@mui/icons-material";
+import Blog from "./blog/Blog";
 
-export default function App() {
-  const [visible, setVisible] = useState(true);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setVisible((prev) => !prev);
-    }, 500);
-    return () => clearInterval(interval);
-  }, []);
+function App() {
 
   return (
     <>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6">Meu App</Typography>
-        </Toolbar>
-      </AppBar>
-      <Container sx={{ textAlign: "center", mt: 4 }}>
-        <Box display="flex" justifyContent="center" alignItems="center" gap={1}>
-          <FlashOn fontSize="large" color="primary" />
-          <Typography
-            variant="h3"
-            gutterBottom
-            sx={{ visibility: visible ? "visible" : "hidden" }}
-          >
-            Meu App
-          </Typography>
-          <FlashOn fontSize="large" color="primary" />
-        </Box>
-        <Typography variant="h6" gutterBottom>
-          Um aplicativo simples utilizando Material UI e React.
-        </Typography>
-        <Box mt={4}>
-          <Button variant="contained" color="primary" href="#">
-            Explorar
-          </Button>
-        </Box>
-        <Box mt={4}>
-          <Link href="https://reactjs.org" target="_blank" rel="noopener noreferrer" variant="h6">
-            Learn React
-          </Link>
-        </Box>
-      </Container>
+      <Blog />
     </>
   );
 }
+
+export default App;
--- src/blog/Blog.tsx
+++ src/blog/Blog.tsx
@@ -1,12 +1,20 @@
 import CssBaseline from '@mui/material/CssBaseline';
 import AppTheme from '../shared-theme/AppTheme';
-import AppAppBar from './components/Header';
+import Content from './components/Content';
+import Header from './components/Header';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 
 export default function Blog(props: { disableCustomTheme?: boolean }) {
   return (
     <AppTheme {...props}>
       <CssBaseline enableColorScheme />
-      <AppAppBar />
+      <Header />
+      <Box sx={{ paddingTop: 'calc(var(--template-frame-height, 0px) + 120px)' }}>
+        <Container maxWidth="lg">
+          <Content />
+        </Container>
+      </Box>
     </AppTheme>
   );
 }
