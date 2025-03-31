7a6cbe42e1f7f875b467fd524ce4ebc31b33cf51
Jcliz <joaopedrocliz@gmail.com>
2025-03-31 16:33:49 -0300

    fix export default

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import React, { useState, useEffect } from "react";
 
 import Blog from "./blog/Blog";
 
-
 function App() {
-
   return (
     <>
       <Blog />
     </>
   );
 }
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './sign-up/SignUp';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
