60aab31584ac0f5bc078fb5a19e4c30511f15bb9
Jcliz <joaopedrocliz@gmail.com>
2025-03-31 15:55:28 -0300

    padronização cadastro

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import SignUp from "./sign-up/SignUp";
+
+function App() {
+
+  return (
+    <>
+      <SignUp />
+    </>
+  );
+}
+
+export default App;
