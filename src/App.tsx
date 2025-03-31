cacc110b296a3b20531a355ba34abb72488d80fd
Jcliz <joaopedrocliz@gmail.com>
2025-03-31 15:53:10 -0300

    padronização

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import login from "./login/login";
+import { Login } from "@mui/icons-material";
+
+function App() {
+
+  return (
+    <>
+      <Login />
+    </>
+  );
+}
+
+export default App;
