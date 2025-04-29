d89d2443b35615c03bdfe3b5680977410bff476a
eduardofabri <eduardo.fabri@wises.com.br>
2025-04-28 22:11:23 -0300

    refactor: mudando path dos arquivos

--- src/blog/Blog.tsx
+++ src/blog/Blog.tsx
@@ -1,7 +1,7 @@
 import CssBaseline from '@mui/material/CssBaseline';
 import AppTheme from '../shared-theme/AppTheme';
-import Content from './components/Content';
-import Header from './components/Header';
+import Content from './Content';
+import Header from './Header';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 
