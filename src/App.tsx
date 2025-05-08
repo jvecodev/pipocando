f679b6b4529a8f04c4390496ba3a29ba43d9e18a
eduardofabri <eduardo.fabri@wises.com.br>
2025-05-07 21:53:47 -0300

    feat: add route com react-router-dom e MainLayout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Blog from "./blog/Blog";
+import Blog from "./features/blog/Blog";
+import SignUp from "./features/sign-up/SignUp";
+import Login from "./features/login/Login";
+import MainLayout from "./layout/MainLayout";
 
 function App() {
   return (
-    <>
-      <Blog />
-    </>
+    <Router>
+      <Routes>
+        {/* Rotas sem Header */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+
+        {/* Rotas com Header (usando MainLayout) */}
+        <Route
+          path="/blog"
+          element={
+            <MainLayout>
+              <Blog />
+            </MainLayout>
+          }
+        />
+        {/* Adicione outras rotas aqui */}
+      </Routes>
+    </Router>
   );
 }
 
