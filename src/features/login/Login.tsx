dbb1ef40d1dd888788ffefe07e3cb451222d23e1
João Pedro Cardoso de Liz <joaopedrocliz@gmail.com>
2025-05-08 09:26:03 -0300

    fix login sroll

--- src/features/login/Login.tsx
+++ src/features/login/Login.tsx
@@ -38,8 +38,8 @@ const Card = styled(MuiCard)(({ theme }) => ({
 }));
 
 const SignInContainer = styled(Stack)(({ theme }) => ({
-  height: 'calc((1 - var(--template-frame-height, 0)) * 100dvh)',
-  minHeight: '100%',
+  minHeight: '100vh',
+  overflowY: 'auto',
   padding: theme.spacing(2),
   [theme.breakpoints.up('sm')]: {
     padding: theme.spacing(4),
